5d76ab55597c6cdf93df2287724c9f772d421621
Holakovský Matyáš <75589615+Gotyxoslav@users.noreply.github.com>
2026-01-22 23:21:24 +0100

    Add files via upload

---
+++ maturita/views/main.py
@@ -0,0 +1,213 @@
+from flask import Flask, request, render_template, redirect, session, url_for, make_response, jsonify, abort
+import uuid
+import os
+import json
+from werkzeug.utils import secure_filename
+
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+
+from sql import get_data, add_data, del_data
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "static", "data")
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "123456789"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.secret_key = "tajnyklic"
+
+ph = PasswordHasher()
+
+def generate_id():
+    return str(uuid.uuid4())
+
+def render_page(page_name, **kwargs):
+    if request.headers.get("HX-Request"):
+        return render_template(page_name, **kwargs) # if I get it right, **kwargs is generally for any number Keyword Arguments (so like songs=songs)
+    else:
+        return render_template("base.html", page_name=page_name, **kwargs)
+    
+
+
+@app.route('/')
+def index():
+    songs = get_data("SONGS_MTR")
+    return render_page("index.html", songs=songs)
+
+# --- Song Management ---
+
+@app.route('/manage-song')
+def manage_song():
+    if "user" in session:
+        role = session["role"]
+    
+        if role == 1 or role == 2:
+            albums = get_data("ALBUMS_MTR")
+            albums = sorted(albums, key=lambda x: x["title"])
+
+            songs = get_data("SONGS_MTR")
+            songs = sorted(songs, key=lambda x: x["title"])
+
+            users = get_data("USERS_MTR")
+            names = {u['id']: u['username'] for u in users}
+
+            return render_page("add_music.html", albums=albums, names=names, songs=songs)
+        
+    else:
+        return redirect(url_for("index"))
+
+@app.route('/add-album', methods=["POST"])
+def add_album():
+    title = request.form.get("title")
+    author = session.get("user")
+    release = request.form.get("release")
+
+    albumfile = request.files["albumfile"]
+    if albumfile.filename.endswith((".png", ".jpg",".jpeg")):
+        album_id = generate_id()
+        filename = album_id + ".png"
+        filepath = os.path.join(app.config["UPLOAD_FOLDER"] + "/albums", filename)
+        albumfile.save(filepath)
+
+        add_data("ALBUMS_MTR", {
+            "album_id": album_id,
+            "title": title,
+            "author": author,
+            "release_date": release,
+            "albumfile": f"albums/{filename}"
+        })
+
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("manage_song"))
+    
+@app.route('/del-album', methods=["POST"])
+def del_album():
+    id = request.form.get("id")
+    songs = get_data("SONGS_MTR")
+    albums = get_data("ALBUMS_MTR")
+
+    for song in songs:
+        if "album" in song and song["album"] == id:
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], song['songfile'])
+            if os.path.exists(filepath):
+                os.remove(filepath)
+
+    for album in albums:
+        if "album_id" in album and album["album_id"] == id: # if "album_id" in album just checks if album_id even exists
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], album['albumfile'])
+            if os.path.exists(filepath):
+                os.remove(filepath)
+
+    del_data("SONGS_MTR", {
+        "album": id
+    })
+    del_data("ALBUMS_MTR", {
+        "album_id": id
+    })
+
+    return redirect(url_for("index"))
+
+@app.route('/add-song', methods=["POST"])
+def add_song():
+    title = request.form.get("title")
+    author = session.get("user")
+    album = request.form.get("album")
+
+    songfile = request.files["songfile"]
+    if songfile.filename.endswith(".mp3"):
+        song_id = generate_id()
+
+        filename = song_id + ".mp3"
+        filepath = os.path.join(app.config["UPLOAD_FOLDER"] + "/songs", filename)
+        songfile.save(filepath)
+        
+        add_data("SONGS_MTR", {
+            "song_id": song_id,
+            "title": title,
+            "author": author,
+            "album": album,
+            "songfile": f"songs/{filename}"
+        })
+
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("manage_song"))
+    
+@app.route('/del-song', methods=["POST"])
+def del_song():
+    id = request.form.get("id")
+    songs = get_data("SONGS_MTR")
+
+    for song in songs:
+        if "song_id" in song and song["song_id"] == id:
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], song['songfile'])
+            if os.path.exists(filepath):
+                os.remove(filepath)
+
+    del_data("SONGS_MTR", {
+        "song_id": id
+    })
+
+    return redirect(url_for("index"))
+
+# --- Login ---
+
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        id = generate_id()
+
+        with open("views\static\data\pfp\Portrait_Placeholder.png", "rb") as f:
+            pfp_img = f.read()
+
+        users = get_data("USERS_MTR")
+        for u in users:
+            if u["email"] == email:
+                return redirect(url_for("login"))
+
+        add_data("USERS_MTR", {
+            "username": username,
+            "email": email,
+            "password": ph.hash(password),
+            "id": id,
+            "pfp": pfp_img
+        })
+        return redirect(url_for("login"))
+
+    return render_template("register.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        users = get_data("USERS_MTR")
+        for u in users:
+            if u["email"] == email:
+                try:
+                    if ph.verify(u["password"], password):
+                        session["user"] = u["id"]
+                        session["role"] = u["role"]
+                        return redirect(url_for("index"))
+                except VerifyMismatchError: # without this, it would spit out an error
+                    pass
+
+        return render_template("login.html", error="Incorrect e-mail or password")
+
+    return render_template("login.html")
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    if "user" in session:
+        session.pop("user", None)
+        session.pop("role", None)
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ maturita/views/sql.py
@@ -0,0 +1,39 @@
+import mysql.connector
+
+def get_connected():
+    return mysql.connector.connect(
+        host="db4free.net",
+        user="toneyadmin",
+        password="qW4rVG58i",
+        database="toneysql"
+    )
+
+mydb = get_connected() 
+mydb.autocommit = True # to znamená že data budou furt aktualizovaný
+mycursor = mydb.cursor(dictionary=True) # dictionary=True dělá že v html se to furt může udávat jako např {{ album.albumfile }}
+
+def get_data(table):
+    sql = f"SELECT * FROM {table}"
+    mycursor.execute(sql)
+    return mycursor.fetchall()
+
+def add_data(table, data):
+    columns = ", ".join(data.keys()) # grabs all the inserted column names (id, name, etc)
+    column_inserts = ", ".join(["%s"] * len(data)) # creates all the insertables (%s) needed for the data
+    column_data = tuple(data.values()) # grabs all the inserted column data (123, John, etc)
+
+    sql = f"INSERT INTO {table} ({columns}) VALUES ({column_inserts})"
+
+    mycursor.execute(sql, column_data)
+    mydb.commit()
+
+def del_data(table, data, logic="AND"):
+    log = f" {logic} " # this is here just so it's " AND " or " OR "
+    requirements = [f"{dat} = %s" for dat in data.keys()] # creates the requirements with column names ("X = %s", "Y = %s")
+    statement = log.join(requirements) # makes the statement ("X = %s AND Y = %s")
+    column_data = tuple(data.values()) # grabs all the inserted column data (123, John, etc)
+
+    sql = f"DELETE FROM {table} WHERE {statement}"
+
+    mycursor.execute(sql, column_data)
+    mydb.commit()
